38b1789f0e66d24b9dc819b6ab1d0ea93a67cceb
豊島秀典 <h193345@edu.tut.ac.jp>
2020-06-02 05:58:27 +0900

    add build file

---
+++ gtd-todo/build/precache-manifest.d67e691eb37046f28c5c7ef30ac136cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6abd3d7859ee385fa0f47331640bdce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b5a2762946c31b6fc10",
+    "url": "/static/css/main.3270b971.chunk.css"
+  },
+  {
+    "revision": "4d849ee04723b0e6ee76",
+    "url": "/static/js/2.f3c5f18c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3c5f18c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b5a2762946c31b6fc10",
+    "url": "/static/js/main.fc8394de.chunk.js"
+  },
+  {
+    "revision": "cf7de208dc3a95010b6f",
+    "url": "/static/js/runtime-main.357287a4.js"
+  }
+]);
