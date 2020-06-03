8a338202cd4e398de0c2d7290cb9dfc247247a07
豊島秀典 <h193345@edu.tut.ac.jp>
2020-06-04 06:14:46 +0900

    add progress bar

--- gtd-todo/src/components/Task.js
+++ gtd-todo/src/components/Task.js
@@ -1,49 +1,45 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Task.css";
-
-const checkedStyle = {
-  background: "#3272fc",
-  "box-shadow": "inset 7px 7px 14px #214aa4 inset -7px -7px 14px #449aff",
-  color: "#dddddd",
-};
 
 const Task = (props) => {
   return (
     <div className={"Task"}>
-      {props.isDone ? (
-        <button
-          className="checkedButton"
-          style={{
-            color: "#3272fc",
-            boxShadow:
-              "15px 15px 30px #bab9c2, -15px -15px 30px #ffffff, inset 2px 2px 5px #bab9c2, inset -2px -2px 5px #ffffff",
-          }}
-          onClick={props.done}
-        >
+      <div className={props.isDone ? "Checked" : "Unchecked"}>
+        <button className="CheckBox" onClick={props.done}>
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
             <path d="M21 6.285l-11.16 12.733-6.84-6.018 1.319-1.49 5.341 4.686 9.865-11.196 1.475 1.285z" />
           </svg>
         </button>
-      ) : (
-        <button className="UncheckedButton" onClick={props.done}>
-          <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M21 6.285l-11.16 12.733-6.84-6.018 1.319-1.49 5.341 4.686 9.865-11.196 1.475 1.285z" />
+        <input
+          type="text"
+          placeholder="new task"
+          onChange={props.change}
+          value={props.name}
+        />
+        <select onChange={props.rankChange} value={props.rank}>
+          <option value="A">A</option>
+          <option value="B">B</option>
+          <option value="C">C</option>
+        </select>
+        <div className="InputNum">
+          <input
+            type="number"
+            onChange={props.taskMinitesChange}
+            value={props.taskMinites}
+          />
+          min
+        </div>
+        <button className="Trash" onClick={props.delete}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <path d="M21 6l-3 18h-12l-3-18h2.028l2.666 16h8.611l2.666-16h2.029zm-4.711-4c-.9 0-1.631-1.099-1.631-2h-5.316c0 .901-.73 2-1.631 2h-5.711v2h20v-2h-5.711z" />
           </svg>
         </button>
-      )}
-      <input type="text" onChange={props.change} value={props.name} />
-      <select onChange={props.rankChange} value={props.rank}>
-        <option value="A">A</option>
-        <option value="B">B</option>
-        <option value="C">C</option>
-      </select>
-      <input
-        type="number"
-        onChange={props.taskMinitesChange}
-        value={props.taskMinites}
-      />
-      min
-      <button onClick={props.delete}>remove</button>
+      </div>
     </div>
   );
 };
