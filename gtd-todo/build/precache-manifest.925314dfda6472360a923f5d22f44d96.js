59378291a4ad74f437251a6b56d2834609f6948f
豊島秀典 <h193345@edu.tut.ac.jp>
2020-06-05 18:26:57 +0900

    change font

--- gtd-todo/build/precache-manifest.925314dfda6472360a923f5d22f44d96.js
+++ gtd-todo/build/precache-manifest.925314dfda6472360a923f5d22f44d96.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "48353cb355c3896e7801baf5621e22cb",
+    "revision": "79c384df0dcead6a0dbe10345e1c5fd2",
     "url": "/ToDo/gtd-todo/build/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ToDo/gtd-todo/build/static/css/2.f524894f.chunk.css"
   },
   {
-    "revision": "b5389c84ef6d359551b1",
-    "url": "/ToDo/gtd-todo/build/static/css/main.6ce734c0.chunk.css"
+    "revision": "0b72d154ff887beaef26",
+    "url": "/ToDo/gtd-todo/build/static/css/main.8471b6b4.chunk.css"
   },
   {
     "revision": "346fd1b541212e730553",
@@ -20,7 +20,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ToDo/gtd-todo/build/static/js/2.ddfb9b2e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b5389c84ef6d359551b1",
+    "revision": "0b72d154ff887beaef26",
     "url": "/ToDo/gtd-todo/build/static/js/main.4f13236d.chunk.js"
   },
   {
