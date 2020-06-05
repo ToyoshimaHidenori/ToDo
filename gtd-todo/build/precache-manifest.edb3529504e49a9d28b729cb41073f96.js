6b6f72337123abced1781a9ecf747b4b652a518e
豊島秀典 <h193345@edu.tut.ac.jp>
2020-06-05 10:59:51 +0900

    fix color and fix sync

---
+++ gtd-todo/build/precache-manifest.edb3529504e49a9d28b729cb41073f96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "578eec33e6d7b5d6d19c009ace3eb2f3",
+    "url": "/ToDo/gtd-todo/build/index.html"
+  },
+  {
+    "revision": "346fd1b541212e730553",
+    "url": "/ToDo/gtd-todo/build/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "0e84376ee2bc6efaa15e",
+    "url": "/ToDo/gtd-todo/build/static/css/main.ea76e815.chunk.css"
+  },
+  {
+    "revision": "346fd1b541212e730553",
+    "url": "/ToDo/gtd-todo/build/static/js/2.ddfb9b2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDo/gtd-todo/build/static/js/2.ddfb9b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e84376ee2bc6efaa15e",
+    "url": "/ToDo/gtd-todo/build/static/js/main.6c756741.chunk.js"
+  },
+  {
+    "revision": "f10ebf388e97e4d1ae95",
+    "url": "/ToDo/gtd-todo/build/static/js/runtime-main.d85d108c.js"
+  }
+]);
--- gtd-todo/build/service-worker.js
+++ gtd-todo/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToDo/gtd-todo/build/precache-manifest.b2c6b73665ec8d155b2756416671bd07.js"
+  "/ToDo/gtd-todo/build/precache-manifest.edb3529504e49a9d28b729cb41073f96.js"
 );
 
 self.addEventListener('message', (event) => {
